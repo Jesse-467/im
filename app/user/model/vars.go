1c18d0ee8fe0d34a500cf8f9e67be4354ae052fb
Jesse <20222005187@m.scnu.edu.cn>
2026-09-19 11:09:54 +0800

    清除旧 go-zero 代码，仓库收敛为三个独立 module
    - 删除 app/（6 个 goctl 服务）、common/、proto/、vendor/ 及其全部残留
    - 删除旧构建与部署产物：Dockerfile、docker-compose.yml、filebeat.yml、
      init.sql、start.sh、start.docker.sh
    - 删除根 go.mod / go.sum，仓库彻底转为 pkg + Account + Chat 三 module 结构
    - 忽略 IDE 调试用环境变量文件（含口令，不入库）

--- app/user/model/vars.go
+++
@@ -1,7 +0,0 @@
-package model
-
-import "github.com/zeromicro/go-zero/core/stores/sqlx"
-
-var ErrNotFound = sqlx.ErrNotFound
-
-const DefaultAvatarUrl = "https://s1.ax1x.com/2022/05/25/XFaDqP.png"
--- common/utils/utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "github.com/google/uuid"
-
-func GenUuid() string {
-	uuid := uuid.New()
-	key := uuid.String()
-	return key
-}
